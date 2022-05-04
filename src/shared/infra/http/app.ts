e0bad910f54d0b5435895194b71dd00c063a5836
Gabriel Ribeiro <55985861+Kaina-Ribeiro@users.noreply.github.com>
2022-05-04 13:16:30 -0300

    Cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -3,6 +3,7 @@ import "dotenv/config";
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
+import cors from "cors";
 import "../../container";
 import { AppError } from "../../errors/AppError";
 import AppDataSource from "../database";
@@ -22,6 +23,7 @@ const app = express();
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(express.json());
+app.use(cors());
 app.use(router);
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
